77245d90a15a56bd19eb8ea7884c318a4a2af118
Hristian Sarmov <hristiansarmov007@gmail.com>
2023-10-18 18:00:41 +0300

    Adding functionality to the main grid
    Added OnAction event for the grid elements, swapping clicked elements and repainting the grid after.

--- project/src/main/java/inarow3/game/GameRulesDialog.java
+++ project/src/main/java/inarow3/game/GameRulesDialog.java
@@ -10,7 +10,7 @@ public class GameRulesDialog {
     /**
      * asd.
      */
-    JDialog showGameRulesDialog(JFrame frame) {
+    static JDialog showGameRulesDialog(JFrame frame) {
         JDialog gameRulesDialog = new JDialog(frame, "Game Rules");
         gameRulesDialog.setVisible(true);
         gameRulesDialog.setSize(200, 200);
