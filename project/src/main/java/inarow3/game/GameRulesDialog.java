938b340cce18ec94154b7809367fc746e39c069a
FleurPetit <f.a.petit@student.tue.nl>
2023-10-23 14:35:40 +0200

    Dialog and Design changes
    The game rules and end game dialog contain all wanted elements. Game rules dialog; game rules and start button, End game dialog; game over message, current score, high score, quit game button and replay button. The buttons are not connected to their function yet. The design of the game is improved by adding a black background and white borders.

--- project/src/main/java/inarow3/game/GameRulesDialog.java
+++ project/src/main/java/inarow3/game/GameRulesDialog.java
@@ -2,42 +2,52 @@ package inarow3.game;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Image;
-import java.awt.Scrollbar;
-
 import javax.swing.*;
 
 /**
-* asd.
+* In this class the game rules dialog is created, which is the first screen you see when opening
+* the game. The rest of the game will not start until the start button on this screen is pressed.
 */
 public class GameRulesDialog {
 
     /**
-     * asd.
+     * Create the game rules dialog, which is a pop-up screen that provides a short 
+     * explanation of how the game works. When the button on the bottom of the screen is pressed
+     * the game starts. The fruits are added to the playingfield after this.
      */
     static JDialog showGameRulesDialog(JFrame frame) {
-        JDialog gameRulesDialog = new JDialog(frame, "Game Rules");
-
+        // Button to start the game.
         JButton startButton = new JButton("Start Game");
-        // startButton.addActionListener();
+        startButton.setBackground(Color.WHITE);
+        startButton.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+        startButton.setBorderPainted(true);
+        // TOdo startButton.addActionListener();
+
+        JDialog gameRulesDialog = new JDialog(frame, "Game Rules");
         
-        JLabel labelRules = new JLabel("the goal of the game is to get three in a row"
-            + " of the same character. this can be done by swapping two charcters,"
-            + " or swapping a character with an empty square."
-            + "once you place three of the same charcters in a row they will disappear."
-            + "every 3 moves, 3 new random characters will appear, "
-            + "making the game harder and harder"
-            + "the goal of the game is to keep the game going as long as possible."
-            + "once no more squares can be added anymore, the game is over");
+        // Game rules, explanation of the game.
+        String labelText = "When clicking on two tiles these tiles will change position. "
+            + "When you possition three of the same fruit in a row, these fruits will disappear. "
+            + "After every 3 moves, 3 extra fruits will be randomly added to the game. "
+            + "The goal is to keep the game going as long as possible. "
+            + "Once no more fruits can be added, you are game over. Are you ready to play?!";
 
-        JPanel panelGameRules = new JPanel();
-        panelGameRules.add(labelRules);
+        // Explanation of the game set to a label.
+        JLabel labelRules = new JLabel("<html>" + labelText + "</html>");
+        labelRules.setVerticalAlignment(JLabel.TOP);
 
+        labelRules.setOpaque(true);
+        labelRules.setBackground(Color.BLACK);
+        labelRules.setForeground(Color.WHITE);
+
+        // Add the label with the game rules and the start button to the dialog, with the button
+        // positioned at the bottom and the text above the button.
         gameRulesDialog.add(labelRules, BorderLayout.CENTER);
         gameRulesDialog.add(startButton, BorderLayout.SOUTH);
 
+        // Set the size of the dialog and adding it to the main frame
         gameRulesDialog.setVisible(true);
-        gameRulesDialog.setSize(200, 200);
+        gameRulesDialog.setSize(225, 225);
         gameRulesDialog.setLocationRelativeTo(frame);
         return gameRulesDialog;
     }
